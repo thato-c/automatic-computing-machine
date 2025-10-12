21b137ab9d160be9a794b6cc18beef3a341fc9ed
Thato <t.chitja369@gmail.com>
2025-10-12 20:57:13 +0200

    Update the Blog Service Page

--- TechRepairs/src/app/services/blog.service.ts
+++ TechRepairs/src/app/services/blog.service.ts
@@ -7,6 +7,15 @@ import { BlogPost } from "../models/blog.model";
 export class BlogService{
     private blogPosts: BlogPost[] = [
         {id: 1, title: 'Introduction to Angular', author: 'Me', date: '2025-08-26', content: 'Angular is a platform for building mobile and desktop web applications...'},
+        {id: 2, title: 'Introduction to TypeScript', author: 'Me', date: '2025-08-26', content: 'Angular is a platform for building mobile and desktop web applications...'},
+        {id: 3, title: 'Introduction to Vue', author: 'Me', date: '2025-08-26', content: 'Angular is a platform for building mobile and desktop web applications...'},
+        {id: 4, title: 'Introduction to React', author: 'Me', date: '2025-08-26', content: 'Angular is a platform for building mobile and desktop web applications...'},
+        {id: 5, title: 'Introduction to Asp.Net', author: 'Me', date: '2025-08-26', content: 'Angular is a platform for building mobile and desktop web applications...'},
+        {id: 6, title: 'Introduction to C#', author: 'Me', date: '2025-08-26', content: 'Angular is a platform for building mobile and desktop web applications...'},
+        {id: 7, title: 'Introduction to Python', author: 'Me', date: '2025-08-26', content: 'Angular is a platform for building mobile and desktop web applications...'},
+        {id: 8, title: 'Introduction to Java', author: 'Me', date: '2025-08-26', content: 'Angular is a platform for building mobile and desktop web applications...'},
+        {id: 9, title: 'Introduction to C', author: 'Me', date: '2025-08-26', content: 'Angular is a platform for building mobile and desktop web applications...'},
+        {id: 10, title: 'Introduction to C++', author: 'Me', date: '2025-08-26', content: 'Angular is a platform for building mobile and desktop web applications...'},
     ];
 
     constructor() {}
